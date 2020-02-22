5245b9cb33246ec89ba36031e4f0754dd9f3b7e1
Seif Kamal <seif@tickx.co.uk>
2020-02-22 21:03:47 +0000

    Updates

---
+++ precache-manifest.b3dae6086b9dab2f7f7374cf4287a5e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc3ec683db0f0eb631b4e8f96a219c3",
+    "url": "/synth/index.html"
+  },
+  {
+    "revision": "9776ce9a853b47bc9964",
+    "url": "/synth/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "5d3386fd2722b45b68cb",
+    "url": "/synth/static/css/main.2e15ccd3.chunk.css"
+  },
+  {
+    "revision": "9776ce9a853b47bc9964",
+    "url": "/synth/static/js/2.2abfd913.chunk.js"
+  },
+  {
+    "revision": "08845d48dc4ea9790083727dd273a86d",
+    "url": "/synth/static/js/2.2abfd913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3386fd2722b45b68cb",
+    "url": "/synth/static/js/main.63fd8afb.chunk.js"
+  },
+  {
+    "revision": "7dd61d90be5bbfc25e04",
+    "url": "/synth/static/js/runtime-main.67ee3189.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/synth/precache-manifest.58bbc4e6aed393352a0cb0ea1fc5cdc9.js"
+  "/synth/precache-manifest.b3dae6086b9dab2f7f7374cf4287a5e8.js"
 );
 
 self.addEventListener('message', (event) => {
